a9c77b8296c49125bb05b228cc1116fb147b162e
meng li <limeng@outlook.fr>
2022-08-01 02:34:35 +0800

    we cool

--- src/components/sided/card.tsx
+++ src/components/sided/card.tsx
@@ -14,7 +14,7 @@ export class Card extends React.Component<CardProps> {
                 ref="card"
                 onMouseEnter={this.hover}
                 onMouseLeave={this.deHover}>
-                <div className={this.props.aff.type === CardType.plain ? "card" : "card-highlighted-" + this.props.aff.affId} >
+                <div className={this.getCardClassName(this.props.aff.type)} >
                     <div className="card-bg"></div>
                     <div className="card-info">
                         <h1>{this.props.aff.name}</h1>
@@ -31,6 +31,9 @@ export class Card extends React.Component<CardProps> {
         super(props);
     }
 
+    private getCardClassName = (type: CardType) => {
+        return type === CardType.plain ? "card" : "card-highlighted-" + this.props.aff.affId;
+    }
     private hover = () => {
         this.props.hover(this.props.aff.affId);
     }
